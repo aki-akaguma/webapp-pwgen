4de6cfbc41c097dba84318939140e9103589a6fa
aki <aki.akaguma@hotmail.com>
2026-04-24 10:18:46 +0900

    updated crates: browserinfocm(0.2.0)

--- src/components/info.rs
+++ src/components/info.rs
@@ -1,29 +1,14 @@
-use browserinfocm::browserinfo::{BroInfo, Browser};
 use browserinfocm::BrowserInfoCm;
+use browserinfocm::BrowserInfoState;
 use dioxus::prelude::*;
 
 /// the component of browser information
 #[component]
 pub fn Info() -> Element {
-    // browser info
-    let broinfo_sig = use_signal(BroInfo::default);
-    let browser_sig = use_signal(Browser::default);
-    let bicmid_sig = use_signal(String::new);
-    let user_sig = use_signal(String::new);
-
-    //let brg = browser_sig.read().clone();
-    //let bim = broinfo_sig.read().clone();
-    /*
-    let bicmid_s = bicmid_sig.read().clone();
-    let user_s = user_sig.read().clone();
-    */
+    // Signals for storing data gathered by BrowserInfoCm.
+    let state_sig = use_signal(BrowserInfoState::default);
 
     rsx! {
-        BrowserInfoCm {
-            broinfo: broinfo_sig,
-            browser: browser_sig,
-            bicmid: bicmid_sig,
-            user: user_sig,
-        }
+        BrowserInfoCm { state: state_sig }
     }
 }
